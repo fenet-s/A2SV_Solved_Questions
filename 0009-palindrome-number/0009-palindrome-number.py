ef8db1401dbb9b14871ad2b67603befc38c96459
fenet-s <fenetseifudin1@gmail.com>
2026-02-04 16:10:01 +0300

    Time: 7 ms (63.55%), Space: 19.3 MB (44.43%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        n = str(x)
+        if n[::-1] == n:
+            return True
+        else:
+            return False
+        
