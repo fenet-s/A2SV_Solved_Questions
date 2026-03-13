8adbebb50aeadbecd09d3b82d7f8ab33bf7e7468
fenet-s <fenetseifudin1@gmail.com>
2026-03-13 21:31:43 +0300

    Add solution for Sum of Square Numbers

---
+++ leetcode/0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,18 @@
+import math
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l = 0
+        r = math.isqrt(c)
+
+        while l <= r:
+            square_sum = l*l + r*r
+
+            if square_sum == c:
+                return True
+            elif square_sum > c:
+                r -= 1
+            else:
+                l += 1
+
+        return False
