c4083f31955196163ea3423e1a0d4f8fc23e049b
fenet-s <fenetseifudin1@gmail.com>
2026-04-16 20:21:19 +0300

    Time: 2 ms (97.95%), Space: 20.7 MB (57.54%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,18 @@
+class Solution:
+    from collections import Counter
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        
+        count = Counter(nums)
+        
+        duplicates = []
+        missing = []
+        
+        n = len(nums)
+        
+        for i in range(1, n + 1):
+            if count[i] == 0:
+                missing.append(i)
+            elif count[i] > 1:
+                duplicates.append(i)
+        
+        return [*duplicates, *missing]
